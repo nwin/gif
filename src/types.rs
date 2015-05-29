51b784802b2085559dc3d3d73dd17ce3c2c2f961
nwin <nwin@users.noreply.github.com>
2015-05-29 14:17:40 +0200

    Add color quantizer

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,10 @@
 //! Common types used both by decoder and encoder
+extern crate color_quant;
+
 use std::mem;
 use std::borrow::Cow;
+
+use util;
 
 /// Disposal method
 #[derive(Debug, Copy, Clone)]
@@ -67,7 +71,7 @@ impl<'a> Default for Frame<'a> {
     fn default() -> Frame<'a> {
         Frame {
             delay: 0,
-            dispose: DisposalMethod::Any,
+            dispose: DisposalMethod::Keep,
             transparent: None,
             needs_user_input: false,
             top: 0,
@@ -78,5 +82,49 @@ impl<'a> Default for Frame<'a> {
             palette: None,
             buffer: Cow::Borrowed(&[])
         }
+    }
+}
+
+impl Frame<'static> {
+    
+    /// Creates a frame from pixels in RGBA format.
+    ///
+    /// Note: This method is not optimized for speed.
+    pub fn from_rgba(width: u16, height: u16, pixels: &mut [u8]) -> Frame<'static> {
+        assert_eq!(width as usize * height as usize * 4, pixels.len());
+        let mut frame = Frame::default();
+        let mut transparent = None;
+        for pix in pixels.chunks_mut(4) {
+            if pix[3] != 0 {
+                pix[3] = 0xFF;
+            } else {
+                transparent = Some([pix[0], pix[1], pix[2], pix[3]])
+            }
+        }
+        frame.width = width;
+        frame.height = height;
+        let nq = color_quant::NeuQuant::new(1, 256, pixels);
+        frame.buffer = Cow::Owned(pixels.chunks(4).map(|pix| nq.index_of(pix) as u8).collect());
+        frame.palette = Some(nq.color_map_rgb());
+        frame.transparent = if let Some(t) = transparent {
+            Some(nq.index_of(&t) as u8)
+        } else {
+            None
+        };
+        frame
+        
+        
+    }
+    
+    /// Creates a frame from pixels in RGB format.
+    ///
+    /// Note: This method is not optimized for speed.
+    pub fn from_rgb(width: u16, height: u16, pixels: &[u8]) -> Frame<'static> {
+        assert_eq!(width as usize * height as usize * 3, pixels.len());
+        let mut vec: Vec<u8> = Vec::with_capacity(pixels.len() + width as usize * height as usize);
+        for v in pixels.chunks(3) {
+            vec.extend([v[0], v[1], v[2], 0xFF].iter().cloned())
+        }
+        Frame::from_rgba(width, height, &mut vec)
     }
 }
